9034d21f4a95d9aa0ef854455f816673b28d21bc
Jun Gu <druidream@gmail.com>
2025-02-26 17:00:43 +0800

    CGFloat with screen width

---
+++ Sources/DrySwiftLibrary/CGFloat.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  DrySwiftLibrary
+//
+//  Created by Jun Gu on 2/26/25.
+//
+
+import UIKit
+
+@MainActor
+public extension CGFloat {
+    static let screenWidth = UIScreen.main.bounds.width
+    static let screenHeight = UIScreen.main.bounds.height
+}
