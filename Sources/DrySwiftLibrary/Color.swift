d98be3fd2f502242ab33de9e52afbb9475b6f3ef
Jun Gu <druidream@gmail.com>
2025-03-11 19:37:45 +0800

    ColorUtils with interpolate

--- Sources/DrySwiftLibrary/Color.swift
+++ Sources/DrySwiftLibrary/Color.swift
@@ -66,3 +66,57 @@ private func parseHexColor(_ hex: String) -> RGBAComponents? {
 
     return RGBAComponents(r: r, g: g, b: b, a: a)
 }
+
+/// Utility functions for color manipulation
+public struct ColorUtils {
+    /// Returns a color that is interpolated between two colors based on progress (0-1)
+    /// - Parameters:
+    ///   - from: The starting color (at progress = 0)
+    ///   - to: The ending color (at progress = 1)
+    ///   - progress: The progress value between 0 and 1
+    /// - Returns: The interpolated color
+    public static func interpolate(from start: Color, to end: Color, progress: Double) -> Color {
+        // Ensure progress is between 0 and 1
+        let t = max(0, min(1, progress))
+
+        // Get color components for both colors
+        let startComponents = start.rgbaComponents
+        let endComponents = end.rgbaComponents
+
+        // Interpolate each component
+        let r = startComponents.r + (endComponents.r - startComponents.r) * CGFloat(t)
+        let g = startComponents.g + (endComponents.g - startComponents.g) * CGFloat(t)
+        let b = startComponents.b + (endComponents.b - startComponents.b) * CGFloat(t)
+        let a = startComponents.a + (endComponents.a - startComponents.a) * CGFloat(t)
+
+        // Create new color from interpolated components
+        return Color(.displayP3,
+                     red: Double(r),
+                     green: Double(g),
+                     blue: Double(b),
+                     opacity: Double(a))
+    }
+}
+
+// MARK: - Private Color Extensions
+private extension Color {
+    /// Extract RGBA components from a Color
+    var rgbaComponents: RGBAComponents {
+        // Need to use UIColor as bridge since SwiftUI's Color doesn't expose components directly
+        let uiColor = UIColor(self)
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+
+        // Get components - defaulting to black transparent if conversion fails
+        if !uiColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
+            // Conversion to RGB failed, try getting white value
+            uiColor.getWhite(&red, alpha: &alpha)
+            green = red
+            blue = red
+        }
+
+        return RGBAComponents(r: red, g: green, b: blue, a: alpha)
+    }
+}
